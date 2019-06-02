8f9c1d546af88ffefe67cf88f04d266dddd566e7
Ahmet Burak Budak <abbudak@users.noreply.github.com>
2019-06-02 21:00:53 +0300

    IF ELSE ELSEIF

---
+++ IF_ELSE/src/main.java
@@ -0,0 +1,17 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+		int x = 20;
+		if (x > 20) {
+			System.out.println("greater than 20");
+		} else if (x == 20) {
+			System.out.println("equals to 20");
+		} else {
+			System.out.println("lower than 20");
+		}
+	}
+
+}
